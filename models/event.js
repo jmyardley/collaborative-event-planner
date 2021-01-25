ce7b364153603467aff536499046e8d2e9ad2738
James Y <35856061+jmyardley@users.noreply.github.com>
2021-01-25 17:57:12 -0500

    event schema modification

--- models/event.js
+++ models/event.js
@@ -5,9 +5,12 @@ const eventSchema = new Schema({
   title: { type: String, required: true },
   date: { type: String, required: true },
   descr: { type: String, required: true },
-  items: { type: Array, required: true}
+  items: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Item'
+}]
 });
 
-const Event = mongoose.model("Event", userSchema);
+const Event = mongoose.model("Event", eventSchema);
 
 module.exports = Event;
