8dc5a652a744742b63503b32e120b4246823bdda
James Y <35856061+jmyardley@users.noreply.github.com>
2021-01-24 21:58:23 -0500

    import edits

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import Header from "./components/Header";
-import Login from "./components/Login";
-import Signup from "./components/Signup";
+import Login from "./components/pages/Login";
+import Signup from "./components/pages/Signup";
+import LandingContainer from "./components/LandingContainer"
 
 
 class App extends Component {
